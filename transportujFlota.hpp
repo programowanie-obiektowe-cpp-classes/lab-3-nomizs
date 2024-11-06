2a06635c35acecbf659e0c25c3eb65ee79a71cde
Szymon <Szymon@DESKTOP-KD7PK0F>
2024-11-06 12:15:23 +0100

    zej3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,24 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+    if (towar==0)
+        return 0;
+    else
+    {
+        int licznik=0; //licznik przetransportowanego sumarycznie towaru
+        int zaglowce=0;
+        
+        while (licznik < towar)
+        {
+            Stocznia stocznia;
+            Statek*  s1 = stocznia();
+            licznik     = licznik + s1->transportuj();
+            if (dynamic_cast<Zaglowiec*>(s1))
+            {
+                zaglowce++;
+            }
+            delete s1;
+        }
+        return zaglowce;
+    }
 }
